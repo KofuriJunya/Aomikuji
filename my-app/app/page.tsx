c67a4ece007558052aaa886e3d699121b62c721b
Butayamaaaaaa! <67731072+KofuriJunya@users.noreply.github.com>
2026-05-03 10:29:31 +0000

    cssサイズを調整4

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -91,8 +91,7 @@ export default function App() {
 
           {/* 動画 */}
           <iframe
-            style={{ width: "100%", maxWidth: "480px", borderRadius: "8px", marginTop: "20px" }}
-            height="270"
+            className="video-iframe"
             src={result?.movie ? toEmbedUrl(result.movie) : ""}
             title="YouTube video player"
             frameBorder="0"
